5d37b7581cf2d3a5d20e21c8cc21a7897f44daa8
Kevin van den Hoek <kevinvandenhoek@gmail.com>
2019-03-11 19:57:50 +0100

    Initial commit

---
+++ Classes/ViewControllers/Splash/SplashViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SplashViewController.swift
+//  ArchitectureChallenge
+//
+//  Created by Kevin van den Hoek on 11/03/2019.
+//  Copyright © 2019 Triple IT. All rights reserved.
+//
+
+import EasyPeasy
+import UIKit
+
+class SplashViewController: UIViewController {
+    
+}
---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArchitectureChallenge
+//
+//  Created by Kevin van den Hoek on 11/03/2019.
+//  Copyright © 2019 Triple IT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
